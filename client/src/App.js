6d01bd6df9ed4a73a9693097aa0b9fd5971110d4
Erich Casagrande <erichfr0zen@gmail.com>
2021-07-20 23:04:24 -0300

    added a temporary data  and remove samplefiles
    added temporary data.
    Removed sample files like CSS, js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  //temporary
+
+  const games = [
+    {
+      name: "BEATS EX",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida ex turpis, a maximus neque volutpat id.",
+      type: "SLOT GAMES",
+      thumbnail: "/01.png",
+      url: "beat-ex",
+      release: "",
+      tags: ["android", "windows"],
+    },
+    {
+      name: "BACCARAT SUPREME",
+      description:
+        "Suspendisse hendrerit diam ac auctor accumsan. Donec blandit metus a diam dictum egestas non non erat.",
+      type: "TABLE GAMES",
+      thumbnail: "/02.png",
+      url: "bacarrat-supreme",
+      release: "",
+      tags: ["android", "html", "ios"],
+    },
+    {
+      name: "LOOT OR BOOT",
+      description:
+        "Curabitur quis sollicitudin odio. Vivamus varius sapien vitae ex lacinia efficitur. ",
+      type: "ARCADE",
+      thumbnail: "/03.png",
+      url: "/loot-or-boot",
+      release: "",
+      tags: ["android", "html", "flash", "ios"],
+    },
+  ];
+
+  return <span>test</span>;
+};
 
 export default App;
