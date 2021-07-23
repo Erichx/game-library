e7d3e6e77aaca0f679c37f6b5cf1874984a4e8f5
Erich Casagrande <erichfr0zen@gmail.com>
2021-07-22 22:37:15 -0300

    Function filter is working
    Now filter is working by using useeffect and triggered by the value selected in select field and using the sort function.

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { GamesContext } from "./Context/GamesContext";
 import Header from "./Components/Header";
 import Content from "./Components/Content";
 import Modal from "./Components/Modal";
-
+const request = [
+  {
+    name: "BEATS EX",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida ex turpis, a maximus neque volutpat id.",
+    type: "SLOT GAMES",
+    thumbnail: "/01.png",
+    url: "beat-ex",
+    release: "Mon Oct 31 2021 00:00:00 GMT-0300",
+    tags: ["Popular"],
+  },
+  {
+    name: "Wild Wild West 2120",
+    description:
+      "Suspendisse hendrerit diam ac auctor accumsan. Donec blandit metus a diam dictum egestas non non erat.",
+    type: "SLOT GAMES",
+    thumbnail: "/02.png",
+    url: "bacarrat-supreme",
+    release: "Mon Oct 22 2021 00:00:00 GMT-0300",
+    tags: [],
+  },
+  {
+    name: "Loot or Boot",
+    description:
+      "Curabitur quis sollicitudin odio. Vivamus varius sapien vitae ex lacinia efficitur. ",
+    type: "ARCADE",
+    thumbnail: "/03.png",
+    url: "/loot-or-boot",
+    release: "Mon Jan 25 2022 00:00:00 GMT-0300",
+    tags: ["Coming soon"],
+  },
+];
 const App = () => {
   //temporary data
 
-  const [games, setGames] = useState([
-    {
-      name: "BEATS EX",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida ex turpis, a maximus neque volutpat id.",
-      type: "SLOT GAMES",
-      thumbnail: "/01.png",
-      url: "beat-ex",
-      release: "2017-10-12T00:00:00",
-      tags: ["Popular"],
-    },
-    {
-      name: "Wild Wild West 2120",
-      description:
-        "Suspendisse hendrerit diam ac auctor accumsan. Donec blandit metus a diam dictum egestas non non erat.",
-      type: "SLOT GAMES",
-      thumbnail: "/02.png",
-      url: "bacarrat-supreme",
-      release: "2021-04-04T00:00:00",
-      tags: [],
-    },
-    {
-      name: "Loot or Boot",
-      description:
-        "Curabitur quis sollicitudin odio. Vivamus varius sapien vitae ex lacinia efficitur. ",
-      type: "ARCADE",
-      thumbnail: "/03.png",
-      url: "/loot-or-boot",
-      release: "2022-01-11T00:00:00",
-      tags: ["Coming soon"],
-    },
-  ]);
+  const [games, setGames] = useState([]);
+
+  const [sortGames, setSortGames] = useState("");
+
+  useEffect(() => {
+    const sortArray = (type) => {
+      const sorted = [...request].sort((a, b) => {
+        switch (type) {
+          case "asc":
+            return a.name.localeCompare(b.name);
+          case "desc":
+            return b.name.localeCompare(a.name);
+          case "newest":
+            return new Date(b.release) - new Date(a.release);
+          case "oldest":
+            return new Date(a.release) - new Date(b.release);
+          default:
+            return null;
+        }
+      });
+      setGames(sorted);
+    };
+
+    sortArray(sortGames);
+  }, [sortGames]);
 
   return (
     <>
-      <GamesContext.Provider value={{ games, setGames }}>
+      <GamesContext.Provider value={{ games, setGames, setSortGames }}>
         <Header />
         <Content games={games} />
         <Modal show="" toggle="" url="" />
--- client/src/Components/SelectSort.js
+++ client/src/Components/SelectSort.js
@@ -3,20 +3,18 @@ import React, { useContext } from "react";
 import { GamesContext } from "../Context/GamesContext";
 
 export default function SelectSort() {
-  const { games, setGames } = useContext(GamesContext);
-
-  console.log("inside select sort", games);
+  const { setSortGames } = useContext(GamesContext);
 
   return (
     <div>
       <label htmlFor="sort">Sort</label>
 
-      <select name="sort" id="sort">
+      <select onChange={(e) => setSortGames(e.target.value)}>
         <option value="">Select</option>
         <option value="asc">Title (A-Z)</option>
         <option value="desc">Title (Z-A)</option>
-        <option value="desc">Date (Newest)</option>
-        <option value="desc">Old (Oldest)</option>
+        <option value="newest">Date (Newest)</option>
+        <option value="oldest">Old (Oldest)</option>
       </select>
     </div>
   );
